6c085d4b247605d8686f7baaba1f11be705b1bf2
isaaccodekill <isaacbello3200@gmail.com>
2020-02-25 08:48:03 +0100

    Added links

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,7 +3,7 @@ import { Redirect } from 'react-router-dom'
 
 const Home = () => {
     return (
-        <Redirect to={"/Operations/Orders"}/>
+        <Redirect to={"/Operations/Production-house"}/>
     );
 };
 
